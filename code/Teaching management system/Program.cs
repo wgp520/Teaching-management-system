dd4b712687e10f62d836d87bb56cbe560df92af2
wgp520 <58548464+wgp520@users.noreply.github.com>
2021-01-09 16:56:43 +0800

    Add files via upload

---
+++ code/Teaching management system/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teaching_management_system
+{
+    static class Program
+    {
+        public static string loginId;
+        public static string loginName;
+        public static bool isTeacher;
+        public static bool startClass;
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
